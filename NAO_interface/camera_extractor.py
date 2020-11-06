7c767feb0d11e3ff36a3e631c20f97b08ab854a0
realgabriele <realgabriele@gmail.com>
2020-11-06 18:49:34 +0100

    documentation and minor fixes
    fixed code after real test on NAO

--- NAO_interface/camera_extractor.py
+++ NAO_interface/camera_extractor.py
@@ -6,8 +6,8 @@
 import sys
 import time
 import redis
-from PIL import Image
 import configuration as cfg
+from PIL import Image
 
 from naoqi import ALProxy
 import vision_definitions
@@ -17,26 +17,38 @@ Zero = None
 Uno = None
 
 red = None
+i = 0
 
 
 def camera_extractor():
-    # naoImage = camera.getImageRemote(nameId)
+    naoImage = camera.getImageRemote(Zero)
     #                               width        height        array
-    # img = Image.frombytes("RGB", (naoImage[0], naoImage[1]), naoImage[6])
+    img = Image.frombytes("RGB", (naoImage[0], naoImage[1]), naoImage[6])
 
-    red.publish("camera0", str(camera.getImageRemote(Zero)))    # upper
-    red.publish("camera1", str(camera.getImageRemote(Uno)))     # lower
+    # perform transformation from PIL to OpenCV image
+    image = img
+    red.rpush("camera0", str(image))
+    red.ltrim("camera0", 0, 9)  # only 10 images queued
 
-    red.set("camera0", str(camera.getImageRemote(Zero)))
-    red.set("camera1", str(camera.getImageRemote(Uno)))
+
+    #red.publish("camera0", str(camera.getImageRemote(Zero)))    # upper
+    #red.publish("camera1", str(camera.getImageRemote(Uno)))     # lower
+
+    #red.set("camera0", str(camera.getImageRemote(Zero)))
+    #red.set("camera1", str(camera.getImageRemote(Uno)))
+    #red.set("cameraid", str(int(red.get("cameraid"))+1))
+    red.publish("camera_notice", "published")
+    print("published")
 
 
 def main():
+    print("main")
     global camera, Zero, Uno
     camera = ALProxy("ALVideoDevice", cfg.NAO_IP, cfg.NAO_PORT)
+
     #                                    id  resolution 320*240       color space                        fps
     Zero = camera.subscribeCamera("Zero", 0, vision_definitions.kVGA, vision_definitions.kRGBColorSpace, 30)
-    Uno  = camera.subscribeCamera("Uno",  1, vision_definitions.kVGA, vision_definitions.kRGBColorSpace, 30)
+    # Uno  = camera.subscribeCamera("Uno",  1, vision_definitions.kVGA, vision_definitions.kRGBColorSpace, 30)
 
     global red
     red = redis.Redis(host=cfg.REDIS_IP, port=cfg.REDIS_PORT)
@@ -44,11 +56,11 @@ def main():
     try:
         while True:
             camera_extractor()
-            time.sleep(0.5)
+            time.sleep(1)
     except KeyboardInterrupt:
         print("Camera: Interrupted, shutting down")
         camera.unsubscribe(Zero)
-        camera.unsubscribe(Uno)
+        #camera.unsubscribe(Uno)
         sys.exit(0)
 
 
--- NAO_interface/configuration.py
+++ NAO_interface/configuration.py
@@ -1,8 +1,7 @@
 """ Configuration of common data to be used by the scripts """
-
 REDIS_IP = "localhost"
 REDIS_PORT = 6379
 
-NAO_IP = "localhost"
-#NAO_PORT = 9559
-NAO_PORT = 54321
+NAO_IP = "192.168.0.104"
+NAO_PORT = 9559
+#NAO_PORT = 54321
