95a059823966d4faa65880aa283561981120942a
realgabriele <realgabriele@gmail.com>
2020-11-08 19:45:38 +0100

    implemented controller with QuLog/TeleoR

---
+++ robot_controller/qulog/nao_interface.py
@@ -0,0 +1,59 @@
+import configuration as cfg
+import redis
+import pickle
+
+
+class NaoInterface:
+    def __init__(self, master):
+        self.master = master
+        self.camera_id = 0
+
+        self.red = redis.Redis(host=cfg.REDIS_IP, port=cfg.REDIS_PORT)
+
+    def execute_command(self, proxy, command, parameters=[]):
+        """ Execute command on NAO robot.
+
+        :param str proxy: NAOqi proxy to be used
+        :param str command: command to be executed
+        :param list parameters: parameters to be passed as arguments to the command function
+        """
+
+        self.red.publish("commands", "" + proxy + "/" + command + ";" + str(parameters) + "")
+
+    def get_events(self):
+        """ Get the events Redis channel.
+
+        :return: Redis channel subscribed to all NAO events
+        :rtype: Redis PubSub
+        """
+
+        sub = self.red.pubsub()
+        sub.subscribe("events")
+        return sub
+
+    def get_response(self):
+        """ Get the response Redis channel.
+
+        :return: Redis channel subscribed to all NAO responses
+        :rtype: Redis PubSub
+        """
+
+        sub = self.red.pubsub()
+        sub.subscribe("response")
+        return sub
+
+    def get_camera(self, camera=0):
+        """ Get a frame from selected camera.
+
+        :return: current frame
+        :rtype: OpenCV image (numpy ndarray)
+        """
+
+        sub = self.red.pubsub()
+        sub.subscribe("camera_notice")
+
+        for message in sub.listen():
+            if message['type'] == 'message':
+                # load OpenCV image
+                image = pickle.loads(self.red.blpop("camera" + str(camera))[1], encoding='bytes')
+                return image
